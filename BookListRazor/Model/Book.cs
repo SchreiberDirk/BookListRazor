6144df3a9221e8690f1dfd33923a81a295acb139
Dirk Schreiber <dirk.schreiber.ds@googlemail.com>
2020-03-19 16:01:21 +0100

    Projekt fertiggestellt

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]       // legt den Id- Wert automatisch fest
+        public int Id { get; set; }
+        [Required]  // macht Name zum pflichtfeld (not Null)
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+    }
+}
---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+
+        public Book Book { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPost()       // IActionResult because we will redirect to another page
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);          // hinzufügen der Daten
+                await _db.SaveChangesAsync();           // Sichern der Daten in Datenbank
+                return RedirectToPage("Index");         // weiterleitung an Index-Seite
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
